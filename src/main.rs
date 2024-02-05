0cf4b12d5367fa7794321e3e7b03e6024ad574f1
S. Bakhmet <157451540+sbakhmet@users.noreply.github.com>
2024-02-05 03:13:09 -0800

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use anchor_client::{solana_client::rpc_client::RpcClient, solana_sdk::{ system_instruction,system_program,commitment_config::CommitmentConfig, signature::Keypair, signer::EncodableKeypair, transaction::Transaction}, *};
+use std::time::Duration;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin)
         .init_resource::<LocalInput>()
+        .init_resource::<PlayerDetails>()
+        .add_systems(PreStartup, (setup))
         // Systems that create Egui widgets should be run during the `CoreSet::Update` set,
         // or after the `EguiSet::BeginFrame` system (which belongs to the `CoreSet::PreUpdate` set).
         .add_systems(Update, ui_example_system)
         .run();
 }
 
-fn ui_example_system(mut contexts: EguiContexts, mut local_input: ResMut<LocalInput>,) {
+pub fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+
+
+
+}
+
+fn ui_example_system(mut contexts: EguiContexts, mut local_input: ResMut<LocalInput>, player_details: Res<PlayerDetails>) {
   
      egui::TopBottomPanel::bottom("input_panel").show(contexts.ctx_mut(), |ui| {
 
@@ -35,7 +47,10 @@ fn ui_example_system(mut contexts: EguiContexts, mut local_input: ResMut<LocalIn
 
 
      egui::SidePanel::right("rightp").show(contexts.ctx_mut(), |ui| {
-        ui.label("Hello World!");
+        ui.label("player_keypair: ");
+        ui.label(&player_details.player_keypair.to_base58_string());
+
+        
      });
      egui::CentralPanel::default().show(contexts.ctx_mut(), |ui| {
         egui::ScrollArea::vertical().show(ui, |ui| {
@@ -47,16 +62,46 @@ fn ui_example_system(mut contexts: EguiContexts, mut local_input: ResMut<LocalIn
 }
 
 
+
+
+
+
 #[derive(Resource)]
 struct LocalInput {
     input_string_buffer: String,
     input_string_sent: String,
     
 }
-
 // custom implementation for unusual values
 impl Default for LocalInput {
     fn default() -> Self {
         LocalInput{input_string_buffer:"".to_string(),input_string_sent:"".to_string(),}
+    }
+}
+
+
+
+#[derive(Resource)]
+struct PlayerDetails {
+    player_keypair: Keypair,
+    player_rpcclient: RpcClient
+
+}
+
+// custom implementation for unusual values
+impl Default for PlayerDetails {
+    fn default() -> Self {
+
+        let url = "https://api.devnet.solana.com::8899".to_string();
+let timeout = Duration::from_secs(1);
+let commitment_config = CommitmentConfig::processed();
+
+
+        
+        PlayerDetails{ player_keypair: Keypair::new(), player_rpcclient: RpcClient::new_with_timeout_and_commitment(
+            url,
+            timeout,
+            commitment_config,
+        ) }
     }
 }
