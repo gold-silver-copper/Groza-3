19a46b28e4364898baf8834f771b0b75c8fa5ae7
S. Bakhmet <157451540+sbakhmet@users.noreply.github.com>
2024-02-05 22:52:25 -0800

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use anchor_client::{
+    solana_client::rpc_client::RpcClient,
+    solana_sdk::{
+        commitment_config::*, pubkey::Pubkey, signature::*, system_instruction,system_transaction, system_program,
+        transaction::Transaction,
+    },
+    *,
+};
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
-use anchor_client::{solana_client::rpc_client::RpcClient, solana_sdk::{    system_instruction,
-    system_program,
-    transaction::Transaction, pubkey::Pubkey,commitment_config::*, signature::*, }, *};
-use std::{str::FromStr,thread, time::Duration};
+use std::{str::FromStr, thread, time::Duration};
 
 fn main() {
     App::new()
@@ -21,155 +26,140 @@ fn main() {
 
 pub fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
-
-
 }
 
-pub fn setup2(mut commands: Commands, mut local_input: ResMut<LocalInput>,  player_details: Res<PlayerDetails>) {
-
+pub fn setup2(
+    mut commands: Commands,
+    mut local_input: ResMut<LocalInput>,
+    player_details: Res<PlayerDetails>,
+) {
     let player_pubkey = player_details.player_keypair.encodable_pubkey();
-    println!("pubkey isssss     {}",player_pubkey.to_string());
+    println!("pubkey isssss     {}", player_pubkey.to_string());
     let payer = &player_details.player_keypair;
 
     let random_pubkey = Pubkey::from_str("G9qpASRsg9xrMjAEoECdAyGj5RB1vrQ3pfWSVk8sBuv8").unwrap();
 
     let client_ref = &player_details.player_rpcclient;
-    
-
-  
 
 
-
-   let space:u64 = 100;
-   let rent = client_ref.get_minimum_balance_for_rent_exemption(space as usize).unwrap();
-   let instr = system_instruction::create_account(
-    &payer.pubkey(),
-    &payer.pubkey(),
-    rent,
-    space,
-    &system_program::ID,
-);
-
-let blockhash = client_ref.get_latest_blockhash().unwrap();
-
-let tx = Transaction::new_signed_with_payer(
-    &[instr],
-    Some(&payer.pubkey()),
-    &[&payer, &payer],
-    blockhash,
-);
-
-let _sig = client_ref.send_and_confirm_transaction(&tx);
+    let blockhash = client_ref.get_latest_blockhash().unwrap();
 
 
-let rpc_response2 = client_ref.request_airdrop(&player_pubkey,10000000000);
-let ten_millis = Duration::from_millis(1000);
-thread::sleep(ten_millis);
-let rpc_response = client_ref.get_account(&player_pubkey);
-
-   match rpc_response2 {
-
-    Ok(acc) => {local_input.output_messages_1.append(&mut vec![ acc.to_string()])},
-    Err(e) =>{local_input.output_messages_1.append(&mut vec![e.to_string()]) }
-   }
-
-   match rpc_response {
-
-    Ok(acc) => {local_input.output_messages_1.append(&mut vec![ acc.lamports.to_string()])},
-    Err(e) =>{local_input.output_messages_1.append(&mut vec![e.to_string()]) }
-   }
-
-  //  local_input
-  
-   
 
 
+
+
+    let rpc_response2 = client_ref.request_airdrop(&player_pubkey, 10000000000);
+    let ten_millis = Duration::from_millis(1000);
+    thread::sleep(ten_millis);
+    let rpc_response3 = client_ref.get_account(&player_pubkey);
+
+    match rpc_response2 {
+        Ok(sign) => local_input
+            .output_messages_1
+            .append(&mut vec![sign.to_string()]),
+        Err(e) => local_input
+            .output_messages_1
+            .append(&mut vec![e.to_string()]),
+    }
+
+    match rpc_response3 {
+        Ok(acc) => local_input
+            .output_messages_1
+            .append(&mut vec![acc.lamports.to_string()]),
+        Err(e) => local_input
+            .output_messages_1
+            .append(&mut vec![e.to_string()]),
+    }
+
+
+    let tx = system_transaction::transfer(&player_details.player_keypair, &random_pubkey, 1000000000, blockhash);
+    let signature = client_ref.send_and_confirm_transaction(&tx);
+
+
+
+
+
+
+    //  local_input
 }
 
-fn ui_example_system(mut contexts: EguiContexts, mut local_input: ResMut<LocalInput>, player_details: Res<PlayerDetails>) {
-  
-     egui::TopBottomPanel::bottom("input_panel").show(contexts.ctx_mut(), |ui| {
-
-        let response = ui.add_sized(ui.available_size(), egui::TextEdit::singleline(&mut local_input.input_string_buffer).desired_rows(3));
+fn ui_example_system(
+    mut contexts: EguiContexts,
+    mut local_input: ResMut<LocalInput>,
+    player_details: Res<PlayerDetails>,
+) {
+    egui::TopBottomPanel::bottom("input_panel").show(contexts.ctx_mut(), |ui| {
+        let response = ui.add_sized(
+            ui.available_size(),
+            egui::TextEdit::singleline(&mut local_input.input_string_buffer).desired_rows(3),
+        );
 
         if response.changed() {
             // …
         }
         if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
             local_input.input_string_sent = local_input.input_string_buffer.clone();
-            local_input.output_messages_1.append(&mut vec!["meow".to_string()]);
+            local_input
+                .output_messages_1
+                .append(&mut vec!["meow".to_string()]);
             local_input.input_string_buffer = "".to_string();
         }
-        
-      
-     });
+    });
 
-
-
-
-
-
-     egui::SidePanel::right("rightp").show(contexts.ctx_mut(), |ui| {
+    egui::SidePanel::right("rightp").show(contexts.ctx_mut(), |ui| {
         ui.label("player_keypair: ");
         ui.label(&player_details.player_keypair.encodable_pubkey().to_string());
-
-        
-     });
-     egui::CentralPanel::default().show(contexts.ctx_mut(), |ui| {
+    });
+    egui::CentralPanel::default().show(contexts.ctx_mut(), |ui| {
         egui::ScrollArea::vertical().show(ui, |ui| {
             ui.label(&local_input.input_string_sent);
             for pot in &local_input.output_messages_1 {
-
                 ui.label(pot);
             }
-            
+
             ui.allocate_space(ui.available_size());
         });
-     });
+    });
 }
-
-
-
-
-
 
 #[derive(Resource)]
 struct LocalInput {
     input_string_buffer: String,
     input_string_sent: String,
-    output_messages_1 : Vec<String>,
-    
+    output_messages_1: Vec<String>,
 }
 // custom implementation for unusual values
 impl Default for LocalInput {
     fn default() -> Self {
-        LocalInput{input_string_buffer:"".to_string(),input_string_sent:"".to_string(),output_messages_1:vec!["".to_string()]}
+        LocalInput {
+            input_string_buffer: "".to_string(),
+            input_string_sent: "".to_string(),
+            output_messages_1: vec!["".to_string()],
+        }
     }
 }
-
-
 
 #[derive(Resource)]
 struct PlayerDetails {
     player_keypair: Keypair,
-    player_rpcclient: RpcClient
-
+    player_rpcclient: RpcClient,
 }
 
 // custom implementation for unusual values
 impl Default for PlayerDetails {
     fn default() -> Self {
-
         let url = "http://127.0.0.1:8899".to_string();
-let timeout = Duration::from_secs(1);
-let commitment_config = CommitmentConfig::processed();
+        let timeout = Duration::from_secs(1);
+        let commitment_config = CommitmentConfig::processed();
 
-
-        
-        PlayerDetails{ player_keypair: Keypair::new(), player_rpcclient: RpcClient::new_with_timeout_and_commitment(
-            url,
-            timeout,
-            commitment_config,
-        ) }
+        PlayerDetails {
+            player_keypair: Keypair::new(),
+            player_rpcclient: RpcClient::new_with_timeout_and_commitment(
+                url,
+                timeout,
+                commitment_config,
+            ),
+        }
     }
 }
